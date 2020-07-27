3bf04a7eb401c0f8278e2f0d11ad127c9c93b8a4
LuoJianwei <luojianwei@pinming.cn>
2020-07-27 12:35:49 +0800

    init

---
+++ src/main/java/com/design/pattern/playground/DesignPatternPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.design.pattern.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternPlaygroundApplication.class, args);
+    }
+
+}
